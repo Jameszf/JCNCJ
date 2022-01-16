11d0dc9bae15599ae3098ac1aed9d494a56a28cd
James Fu <fzjames2037@gmail.com>
2022-01-15 17:23:13 -0700

    Routing + Templates.

--- mysite/music_board/urls.py
+++ mysite/music_board/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    path('home/', views.index),
+    path('path/', views.play),
+    path('profile/', views.profile),
+    path('create/', views.create),
 ]
--- mysite/music_board/views.py
+++ mysite/music_board/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello from Music Board!")
+
+
+
+def profile(request):
+    return HttpResponse("Profile")
+
+
+
+def create(request):
+    return render(request, "create.html")
